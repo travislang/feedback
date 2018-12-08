93dac57a0a6d55b850b1746260b0cbced7388307
Travis Lang <tlang50@icloud.com>
2018-12-08 15:04:03 -0600

    install dependencies and setup router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Feedback!</h1>
+                </header>
+            </div>
+            <Route path="/" exact component={Home}></Route>
+        </Router>
+      
     );
   }
 }
